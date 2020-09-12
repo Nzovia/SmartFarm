39c74d94f3997f6034505a8d1ef9f1628272d2ed
root <root@localhost.localdomain>
2020-09-12 05:02:57 +0300

    This a simple android app aimed at showing how to undertake different farming procedures on different farming types.It contains bottom naviagation with home, actvities, and notifications icons  this will help the user navigate through different app activities for the user to have a proper utililization of the app's service

---
+++ app/src/main/java/com/programmer/smartfarm/FarmUpdates.java
@@ -0,0 +1,14 @@
+package com.programmer.smartfarm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FarmUpdates extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_farm_updates);
+    }
+}
