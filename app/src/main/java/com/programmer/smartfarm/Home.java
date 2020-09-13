cda716ea86a6feb9aa86e1d1e87ee0edca9f871f
Nickie19 <nickiemaundu19@gmail.com>
2020-09-14 01:12:59 +0300

    Toolbar created with navigation icon ,once clicked it brings the drawer to the front.On the fragments the home fragments has been updated with sample crops to be considered

--- app/src/main/java/com/programmer/smartfarm/Home.java
+++ app/src/main/java/com/programmer/smartfarm/Home.java
@@ -1,7 +1,10 @@
 package com.programmer.smartfarm;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
@@ -9,13 +12,37 @@ import android.view.MenuItem;
 import android.widget.Switch;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
 
 public class Home extends AppCompatActivity {
+    //variables
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        //the Hooks press Ctrl +D to duplicate them then change the id's
+        drawerLayout=findViewById(R.id.drawer);
+        navigationView=findViewById(R.id.navigationView);
+        toolbar=findViewById(R.id.toolbar1);
+
+        //toolbar code
+        setSupportActionBar(toolbar);
+
+        //navigation drawer code then pass this keyword and drawer layout
+        navigationView.bringToFront();
+        ActionBarDrawerToggle toggle=new ActionBarDrawerToggle(this,drawerLayout,toolbar,
+                R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        //this sets the naviagation items clickable
+        //navigationView.setNavigationItemSelectedListener(this);
 
         //In this create a hook to bottom navigation activity
         BottomNavigationView bottomNavigationView=findViewById(R.id.appnavigation);
--- app/src/main/java/com/programmer/smartfarm/HomeFragment.java
+++ app/src/main/java/com/programmer/smartfarm/HomeFragment.java
@@ -4,15 +4,20 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toolbar;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 
 public class HomeFragment extends Fragment {
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_home,container,false);
+
     }
 }
